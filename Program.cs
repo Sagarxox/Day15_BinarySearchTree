c1a153365eba14c4f0b0e8a05045397b37534b69
sagar <skatahre@gmail.com>
2022-12-16 23:36:57 +0530

    binary search tree

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using BST;
+using System.Text;
+using System.Threading.Tasks;
+
+public class program
+{
+    public static void Main (string[] args)
+    {
+        BST.BinarySearchTree<int> tree = new BST.BinarySearchTree<int>(56);
+        tree.AddNode(30);
+        tree.AddNode(70);
+        tree.Display();
+
+    }
+}
+  
